84c2cfabf61c90f89d71a4968c21243a29a267e8
Nick White <bbycinka@gmail.com>
2022-10-26 21:22:35 +0300

    feat:(frontend-1): установка пакетов: REDUX,AXIOS,CHARTJS,CLASSNAMES,REACT_ROUTER-DOM

---
+++ team-one/src/components/App/App.jsx
@@ -0,0 +1,9 @@
+import stlyesApp from './App.module.css';
+export const App = () => {
+  return (
+    <div className={stlyesApp.app}>
+    </div>
+  );
+}
+
+export default App;
--- team-one/src/index.js
+++ team-one/src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { App } from "./components/App/App";
+import {store} from './service/store/index';
+import { Provider } from "react-redux";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+  <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
-
-reportWebVitals();
