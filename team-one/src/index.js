88486a1875876f4b24c4f5af783876990c0cdafd
Nick White <bbycinka@gmail.com>
2022-10-26 21:38:21 +0300

    feat:(frontend-1): Настроен Route

--- team-one/src/index.js
+++ team-one/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { App } from "./components/App/App";
 import {store} from './service/store/index';
+import { BrowserRouter as Router} from 'react-router-dom'
 import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
   <Provider store={store}>
-    <App />
+  <Router>
+  <App />
+  </Router>
     </Provider>
   </React.StrictMode>
 );
---
+++ team-one/src/pages/RecomendationPage/RecomendationPage.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom"
+
+export const RecomendationPage = () => {
+    return (
+        <div>
+            <h1>RecomendationPage</h1>
+            <Link to="/login">132</Link>
+        </div>
+    )
+}
