24412002da3789708613741a5f548bd2d67f3a1f
rmatafonov <matafonov.rv@gmail.com>
2022-11-01 18:14:24 +0300

    Джоайн импорта и экспорта

--- ds/customs_data_fetcher.py
+++ ds/customs_data_fetcher.py
@@ -11,13 +11,34 @@ import requests
 
 class CustomsDataFetcher:
     __config: Dict[str, Optional[str]]
-    __df_customs: pd.DataFrame
+    __payload: str
+    __headers = {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
+    }
 
-    def __init__(self, config: Dict[str, Optional[str]]):
+    def __init__(self, config: Dict[str, Optional[str]], period_start: str, period_end: str, direction: str):
         self.__config = config
-        self.load_customs_data()
+        self.__payload = json.dumps({
+            "periodTab": "YY",
+            "period": [
+                {
+                    "start": period_start,
+                    "end": period_end
+                }
+            ],
+            "direction": direction,
+            "countries": [],
+            "tnved": [],
+            "tnvedLevel": 6,
+            "federalDistricts": [],
+            "subjects": [],
+            "costForm": 1,
+            "weightForm": 1
+        })
 
-    def load_customs_data(self):
+    def get_df(self, fetch_pages_count=None) -> pd.DataFrame:
         url = f'{self.__config["CUSTOMS_STATS_URL"]}?page=1&pageSize=100'
         init_response = requests.post(url=url, data=self.__payload, headers=self.__headers)
         if init_response.status_code != 200:
@@ -26,11 +47,12 @@ class CustomsDataFetcher:
         init_json = init_response.json()
         df = pd.DataFrame(
             data=init_json["data"],
-            columns=["tnved", "period", "direction", "subject", "dei"]
+            columns=["tnved", "period", "subject", "dei"]
         )
+        fetch_pages = init_json["pageCount"] + 1 if fetch_pages_count is None else fetch_pages_count
         executor = ThreadPoolExecutor(4)
         futures = {
-            executor.submit(self.__load_page, page): page for page in list(range(2, init_json["pageCount"] + 1))
+            executor.submit(self.__load_page, page): page for page in list(range(2, fetch_pages))
         }
         dataframes = [df]
         for future in concurrent.futures.as_completed(futures):
@@ -42,8 +64,8 @@ class CustomsDataFetcher:
             else:
                 dataframes.append(data)
 
-        self.__df_customs = pd.concat(dataframes).groupby(["tnved", "period"])[["dei"]].aggregate(np.sum).reset_index()
-        print(self.__df_customs)
+        grouped = pd.concat(dataframes).groupby(["tnved", "period", "subject"])
+        return grouped[["dei"]].aggregate(np.sum).reset_index()
 
     def __load_page(self, page: int) -> pd.DataFrame:
         url = f'{self.__config["CUSTOMS_STATS_URL"]}?page={page}&pageSize=100'
@@ -57,35 +79,8 @@ class CustomsDataFetcher:
         print(f"[Thread - {threading.current_thread().ident}] Response json {response_json}")
         df_response = pd.DataFrame(
             data=response_json["data"],
-            columns=["tnved", "period", "direction", "subject", "dei"]
+            columns=["tnved", "period", "subject", "dei"]
         )
-        aggregated = df_response.groupby(["tnved", "period", "subject", "direction"])[["dei"]].aggregate(np.sum).reset_index()
-        aggregated.loc[aggregated["direction"] == "Экспорт", "dei"] *= -1
-        difference = aggregated.groupby(["tnved", "period", "subject"])[["dei"]].aggregate(np.sum).reset_index()
-        return difference
-
-    def get_df(self) -> pd.DataFrame:
-        return self.__df_customs
-
-    __headers = {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
-    }
-
-    __payload = json.dumps({
-        "periodTab": "YY",
-        "period": [
-            {
-                "start": "2021-12-31",
-                "end": "2022-12-31"
-            }
-        ],
-        "countries": [],
-        "tnved": [],
-        "tnvedLevel": 6,
-        "federalDistricts": [],
-        "subjects": [],
-        "costForm": 1,
-        "weightForm": 1
-    })
+        grouped = df_response.groupby(["tnved", "period", "subject"])
+        aggregated = grouped[["dei"]].aggregate(np.sum).reset_index()
+        return aggregated
--- ds/main.py
+++ ds/main.py
@@ -3,12 +3,20 @@ from dotenv import dotenv_values
 from customs_data_fetcher import CustomsDataFetcher
 from mongo_cient import TeamOneMongoClient
 from okpd_tnved_dictionary import OkpdTnvedDictionary
+from worker import Worker
 
 if __name__ == '__main__':
     config = dotenv_values(".env")
-    okpd_tnved_dictionary = OkpdTnvedDictionary(config)
-    print(okpd_tnved_dictionary.get_tnveds("01.11.12"))
-    customs_data_fetcher = CustomsDataFetcher(config)
+    worker = Worker(
+        config=config,
+        import_data_fetcher=CustomsDataFetcher(
+            config=config, period_start="2021-12-31", period_end="2022-12-31", direction="ИМ"
+        ),
+        export_data_fetcher=CustomsDataFetcher(
+            config=config, period_start="2021-12-31", period_end="2022-12-31", direction="ЭК"
+        ),
+        okpd_tnved_dictionary=OkpdTnvedDictionary(config=config)
+    )
     mongo = TeamOneMongoClient(config)
-    mongo.save_customs_stats(customs_data_fetcher.get_df())
+    mongo.save_customs_stats(worker.get_df(fetch_pages_count=4))
     mongo.destroy()
